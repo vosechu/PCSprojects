7c813b132876ebd5dc82a18fab3b5f36977626f2
keighty <k80.leonard@gmail.com>
2013-06-03 17:41:14 -0700

    hw1

---
+++ homework/wk1hw.rb
@@ -0,0 +1,24 @@
+# What is your favorite color?
+puts "my favourite color is red"
+
+# How old are you in seconds? (If you did this with only a calculator how would you do it?)
+puts "birthday Aug 12, 1977"
+puts "today's date: "
+puts Time.now
+# What is a String?
+# What’s the difference between single and double-quoted strings?
+# How do you use a quote mark inside of a string? (escaping characters)
+# What is a variable?
+# How do you put a variable inside a string? (interpolation or concatenation)
+# What does the to_s or the to_i method do and why do we need them? (type casting)
+# Evens:
+
+# How do you call a method on a variable?
+# How do you generate a random number between 1 and 20?
+# How do you define a new method?
+# How do you define a new method that takes 2 parameters?
+# What is a method?
+# What happens if you specify that it needs 2 parameters but you only give it 1? (arity)
+# If you use a variable outside a function can it be used inside?
+# If you assign a new value inside the function, what is the value of the variable after the function? (overwriting vs shadowing)
+# If you don’t explicitly return a value from a function, what is returned? (implicit return)
