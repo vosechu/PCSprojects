318f20b3dcdff3c8d397882c4cef76b42407b0c0
keighty <k80.leonard@gmail.com>
2013-06-10 22:08:32 -0700

    homework for week 2 - tuesday

--- homework/wk2hw1.rb
+++ homework/wk2hw1.rb
@@ -1,11 +1,36 @@
 # When is it acceptable to use CamelCase?
 puts "use CamelCase when naming classes"
 # When do we use do...end instead of {...}?
+puts "use do end instead of {} when your block needs to be on multiple lines"
+
 # In Test::Unit, what is the difference between assert and assert_equal?
+puts "assert fails unless test is a true value"
+puts "assert_equal fails unless expected val == actual val"
+
 # Where do we put code that needs to be run before or after each test?
+puts %|
+def setup
+end
+|
+
 # When we use setup, why do we need to use instance variables?
+puts "you use instance variables to enable use of the var in subsequent methods"
+
 # How do you run all the specs in a folder?
+puts "spec ."
+
 # What is a stub?
+puts "a stub is an object that implements the same interface as one of the supporting objects but returns the values we want"
+
 # What is a mock?
+puts "a mock is an object that knows which methods should be called and with what arguments"
+
 # Why do we make sure that our tests are red before fixing them?
+puts %|
+def test_with_descriptive_title
+  flunk("failure message, if you want one")
+end
+|
+
 # If your company won’t allow full tests, what does it mean to “exercise your code, even just a little bit.”
+puts "write a bare minimum unit test with no asserts at all. It will still tell you a lot"
--- homework/wk2hw2.rb
+++ homework/wk2hw2.rb
@@ -1,13 +1,42 @@
 # What alternative keyword can we use if we’re negating the boolean expression inside our if?
+puts "unless"
+
 # What is the ‘negative doppleganger’ for while?
+puts "until"
+
 # What is a “modifier form” of an expression? (Which I call a post-conditional)
+puts "@title = new_title unless @read_only"
+
 # What does a case statement return by default?
+puts "case statement returns the values of nil if no when clause matches and there is no else"
+
 # What two values evaluate to false inside an if?
+puts "false and nil"
+
 # What is a ternary expression used for (?:)?
+puts "a compact if statement with the condition part coming before the question mark"
+
 # What is a “guarded or” used for (||=)?
+puts "a guarded or is for ensuring initialization of a variable to a value ( ||= ) "
+
 # What’s the fastest way to make an array of words?
+puts %|
+%w{twinkle twinkle little star}
+|
 # When using symbols as Hash keys, what are the two ways to initialize a Hash using the curly brace notation? (1.9 hash syntax)
+puts "hash = {'a' => 'cat', 'b' => 'dog'}"
+puts "hash = { a: 'cat', b: 'dog'}"
+
 # What is the name of the * operator?
+puts "* is the splat operator"
+
 # Should you use a for loop to iterate over an Array or Hash? If not, what’s the alternative?
+puts "you should use each to loop over an array"
+puts "using a for loop will just call each in the background"
+
 # What is the difference between each and map?
+puts "map will modify each element in the array"
+puts "each will return each element in the array"
+
 # What is a bang method?
+puts "a bang method does something unexpected -- it may modify the contents of the variable in place"
